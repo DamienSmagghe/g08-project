ea77a2b12e166bfb13fc202554704d4bcc982ebf
DamienSmagghe <damien.smagghe@hetic.net>
2018-12-18 16:45:00 +0100

    :heavy_minus_sign: Remove Renderer dependencie

--- g08-project/src/components/Questionnary/Questionnary.vue
+++ g08-project/src/components/Questionnary/Questionnary.vue
@@ -26,7 +26,6 @@
         earthViability,
         populationViability
     } from './viabilities.js'
-    import Renderer from '../world/components/Renderer.js'
 
     export default {
         name: 'Questionnary',
